72d5fde70b69b1c0452dc2cdffb79cdc8d0b593f
YUVARANI JAGATHESAN <127472925+Rach0613@users.noreply.github.com>
2025-04-18 20:31:10 +0800

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,24 @@
+import "package:flutter/material.dart";
+//import "package:ui_skeleton/screens/onboarding_screen.dart";
+//import "screens/created_account_screen.dart";
+//import "screens/log_in_user.dart";
+//import "screens/admin_login.dart";
+//import "screens/forget_pwd.dart";
+//import "screens/reset_pwd.dart";
+//import "screens/create_acc.dart";
+//import "screens/search.dart";
+//import "screens/global_search.dart";
+//import "screens/explore.dart";
+
+
+// everytime create screen must import here 1st !
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CreateAcc(),
+    ),
+  );
+}
+
